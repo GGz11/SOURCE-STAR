92aa4af3a3b16c059a33a90cc7dcffa95fde85c1
GGz11 <oa176304@gmail.com>
2024-06-07 23:17:20 +0300

    Add files via upload

--- config (2).py
+++ config (2).py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/XBDHJG/Al-Qahrawi.git",
+    "https://github.com/GGz11/SOURCE-STAR.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
